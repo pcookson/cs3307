7f9baf60ed2c5d5d9cab9b771a7f57e611c426ad
james <james@bluebaron.ca>
2014-12-03 18:44:27 -0500

    Unadded comments for payment.h

--- src/Payment/payment.h
+++ src/Payment/payment.h
@@ -14,26 +14,34 @@
 #include "../Db/db.h"
 #include "../User/user.h"
 
-
+/**
+ * Container for the information of a single payment
+ */
 class Payment {
 public:
+	// @brief Creates a new Payment object
 	Payment();
+
+	// @brief Deletes a Payment object
 	virtual ~Payment();
 
+	// the date and time transaction occurred
 	std::string date_time;
 
-	// An integer of the amount including cents with no decimal places.  Divide by 100 for human readability.
+	// the amount of the payment
 	int amount;
 
+	// the id of the payment
 	std::string id;
 
+	// the account the payment was made on
 	Account account;
 
+	// the user that owns the account the payment was made on
 	User::User user;
-
-
 };
 
+// @brief A collection container for multiple payments
 typedef std::vector<Payment> Payments;
 
 #endif /* PAYMENT_H_ */
